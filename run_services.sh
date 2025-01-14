84401a20484c1c9982dc33084a2e656a9739afac
Lena Aretz <aretzlena@gmail.com>
2025-01-14 11:01:40 +0100

    refactored file structure

--- run_services.sh
+++ run_services.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 export PYTHONPATH=/app/src:$PYTHONPATH
-uvicorn api:app --host 0.0.0.0 --port 8081 & 
+uvicorn src.api.api:app --host 0.0.0.0 --port 8081 &
 streamlit run src/streamlit_app.py --server.port 8080 --server.address 0.0.0.0
